7a2cfdb04fa777651f729e0a3addcce6d221cde7
Alex Boylan <aboylan@users.noreply.github.com>
2024-03-11 21:12:18 -0600

    first commit

---
+++ src/test/java/com/aboylan/curso/springboot/app/interceptor/springbootinterceptor/SpringbootInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aboylan.curso.springboot.app.interceptor.springbootinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
